0413b41c1223d1773103e61d967562971da73b11
Morris Mulitu <37964707+MorrisMuuoMulitu@users.noreply.github.com>
2024-04-28 14:44:38 +0000

    damn, no heroes

--- js/hero.js
+++ js/hero.js
@@ -1,136 +1,184 @@
-// Smooth Scroll
-const lenis = new Lenis()
-lenis.on('scroll', (e) => {
-    console.log(e)
-})
-function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-}
-requestAnimationFrame(raf)
+(function() {
+
+  var $$ = function(selector, context) {
+    var context = context || document;
+    var elements = context.querySelectorAll(selector);
+    return [].slice.call(elements);
+  };
+
+  function _fncSliderInit($slider, options) {
+    var prefix = ".fnc-";
+
+    var $slider = $slider;
+    var $slidesCont = $slider.querySelector(prefix + "slider__slides");
+    var $slides = $$(prefix + "slide", $slider);
+    var $controls = $$(prefix + "nav__control", $slider);
+    var $controlsBgs = $$(prefix + "nav__bg", $slider);
+    var $progressAS = $$(prefix + "nav__control-progress", $slider);
+
+    var numOfSlides = $slides.length;
+    var curSlide = 1;
+    var sliding = false;
+    var slidingAT = +parseFloat(getComputedStyle($slidesCont)["transition-duration"]) * 1000;
+    var slidingDelay = +parseFloat(getComputedStyle($slidesCont)["transition-delay"]) * 1000;
+
+    var autoSlidingActive = false;
+    var autoSlidingTO;
+    var autoSlidingDelay = 5000; // default autosliding delay value
+    var autoSlidingBlocked = false;
+
+    var $activeSlide;
+    var $activeControlsBg;
+    var $prevControl;
+
+    function setIDs() {
+      $slides.forEach(function($slide, index) {
+        $slide.classList.add("fnc-slide-" + (index + 1));
+      });
+
+      $controls.forEach(function($control, index) {
+        $control.setAttribute("data-slide", index + 1);
+        $control.classList.add("fnc-nav__control-" + (index + 1));
+      });
+
+      $controlsBgs.forEach(function($bg, index) {
+        $bg.classList.add("fnc-nav__bg-" + (index + 1));
+      });
+    };
+
+    setIDs();
+
+    function afterSlidingHandler() {
+      $slider.querySelector(".m--previous-slide").classList.remove("m--active-slide", "m--previous-slide");
+      $slider.querySelector(".m--previous-nav-bg").classList.remove("m--active-nav-bg", "m--previous-nav-bg");
+
+      $activeSlide.classList.remove("m--before-sliding");
+      $activeControlsBg.classList.remove("m--nav-bg-before");
+      $prevControl.classList.remove("m--prev-control");
+      $prevControl.classList.add("m--reset-progress");
+      var triggerLayout = $prevControl.offsetTop;
+      $prevControl.classList.remove("m--reset-progress");
+
+      sliding = false;
+      var layoutTrigger = $slider.offsetTop;
+
+      if (autoSlidingActive && !autoSlidingBlocked) {
+        setAutoslidingTO();
+      }
+    };
+
+    function performSliding(slideID) {
+      if (sliding) return;
+      sliding = true;
+      window.clearTimeout(autoSlidingTO);
+      curSlide = slideID;
+
+      $prevControl = $slider.querySelector(".m--active-control");
+      $prevControl.classList.remove("m--active-control");
+      $prevControl.classList.add("m--prev-control");
+      $slider.querySelector(prefix + "nav__control-" + slideID).classList.add("m--active-control");
+
+      $activeSlide = $slider.querySelector(prefix + "slide-" + slideID);
+      $activeControlsBg = $slider.querySelector(prefix + "nav__bg-" + slideID);
+
+      $slider.querySelector(".m--active-slide").classList.add("m--previous-slide");
+      $slider.querySelector(".m--active-nav-bg").classList.add("m--previous-nav-bg");
+
+      $activeSlide.classList.add("m--before-sliding");
+      $activeControlsBg.classList.add("m--nav-bg-before");
+
+      var layoutTrigger = $activeSlide.offsetTop;
+
+      $activeSlide.classList.add("m--active-slide");
+      $activeControlsBg.classList.add("m--active-nav-bg");
+
+      setTimeout(afterSlidingHandler, slidingAT + slidingDelay);
+    };
 
 
-// Scroll Down - Button
-document.addEventListener('DOMContentLoaded', function () {
-    const scrollBtn = document.getElementById('scrollBtn');
-    window.addEventListener('scroll', function () {
-        const box = document.querySelector('.scrollBtn');
-        if (window.scrollY > 0) {
-            box.classList.add('move');
-        } else {
-            box.classList.remove('move');
-        }
+
+    function controlClickHandler() {
+      if (sliding) return;
+      if (this.classList.contains("m--active-control")) return;
+      if (options.blockASafterClick) {
+        autoSlidingBlocked = true;
+        $slider.classList.add("m--autosliding-blocked");
+      }
+
+      var slideID = +this.getAttribute("data-slide");
+
+      performSliding(slideID);
+    };
+
+    $controls.forEach(function($control) {
+      $control.addEventListener("click", controlClickHandler);
     });
+
+    function setAutoslidingTO() {
+      window.clearTimeout(autoSlidingTO);
+      var delay = +options.autoSlidingDelay || autoSlidingDelay;
+      curSlide++;
+      if (curSlide > numOfSlides) curSlide = 1;
+
+      autoSlidingTO = setTimeout(function() {
+        performSliding(curSlide);
+      }, delay);
+    };
+
+    if (options.autoSliding || +options.autoSlidingDelay > 0) {
+      if (options.autoSliding === false) return;
+      
+      autoSlidingActive = true;
+      setAutoslidingTO();
+      
+      $slider.classList.add("m--with-autosliding");
+      var triggerLayout = $slider.offsetTop;
+      
+      var delay = +options.autoSlidingDelay || autoSlidingDelay;
+      delay += slidingDelay + slidingAT;
+      
+      $progressAS.forEach(function($progress) {
+        $progress.style.transition = "transform " + (delay / 1000) + "s";
+      });
+    }
+    
+    $slider.querySelector(".fnc-nav__control:first-child").classList.add("m--active-control");
+
+  };
+
+  var fncSlider = function(sliderSelector, options) {
+    var $sliders = $$(sliderSelector);
+
+    $sliders.forEach(function($slider) {
+      _fncSliderInit($slider, options);
+    });
+  };
+
+  window.fncSlider = fncSlider;
+}());
+
+/* not part of the slider scripts */
+
+/* Slider initialization
+options:
+autoSliding - boolean
+autoSlidingDelay - delay in ms. If audoSliding is on and no value provided, default value is 5000
+blockASafterClick - boolean. If user clicked any sliding control, autosliding won't start again
+*/
+fncSlider(".example-slider", {autoSlidingDelay: 4000});
+
+var $demoCont = document.querySelector(".demo-cont");
+
+[].slice.call(document.querySelectorAll(".fnc-slide__action-btn")).forEach(function($btn) {
+  $btn.addEventListener("click", function() {
+    $demoCont.classList.toggle("credits-active");
+  });
 });
 
-
-// Preloader Text 
-window.addEventListener('load', function () {
-    const preloader = document.getElementById('preloader');
-    const firstText = document.getElementById('first-text');
-    const secondText = document.getElementById('second-text');
-    // Show the first text
-    firstText.style.opacity = '1';
-    // loading animation
-    setTimeout(function () {
-        firstText.style.opacity = '0';
-        secondText.style.opacity = '1';
-    }, 1000);
-    setTimeout(function () {
-        preloader.style.display = 'none';
-    }, 4000);
+document.querySelector(".demo-cont__credits-close").addEventListener("click", function() {
+  $demoCont.classList.remove("credits-active");
 });
 
-
-// Loader Video 
-window.addEventListener('load', function () {
-    document.body.classList.add('overflow-hidden'); // body - overflow hidden
-    document.documentElement.classList.add('overflow-hidden'); // html - overflow hidden
-    setTimeout(function () {
-        const loaderVideo = document.getElementById('loaderVideo');
-        loaderVideo.style.width = '90%';
-        loaderVideo.style.height = '90%';
-        loaderVideo.style.transform = 'translate(-50%, -50%)';
-        loaderVideo.style.top = '50%';
-        loaderVideo.style.left = '50%';
-        loaderVideo.style.position = 'fixed';
-        loaderVideo.style.borderRadius = '12px';
-
-    }, 2000);
-    setTimeout(() => {
-        if (window.matchMedia('(max-width: 576px)').matches) {
-            loaderVideo.style.width = '220px';
-            loaderVideo.style.height = '220px';
-            loaderVideo.style.top = '25%';
-            loaderVideo.style.left = '24px';
-            loaderVideo.style.right = 'auto';
-            loaderVideo.style.transform = 'translate(0%, -25%)';
-        } else if (window.matchMedia('(max-width: 767px)').matches) {
-            loaderVideo.style.width = '220px';
-            loaderVideo.style.height = '220px';
-            loaderVideo.style.left = 'auto';
-            loaderVideo.style.right = '40px';
-            loaderVideo.style.transform = 'translate(0%, -50%)';
-        } else if (window.matchMedia('(max-width: 991px)').matches) {
-            loaderVideo.style.width = '310px';
-            loaderVideo.style.height = '310px';
-            loaderVideo.style.left = 'auto';
-            loaderVideo.style.right = '40px';
-            loaderVideo.style.transform = 'translate(0%, -50%)';
-        } else if (window.matchMedia('(max-width: 1199px)').matches) {
-            loaderVideo.style.width = '400px';
-            loaderVideo.style.height = '400px';
-            loaderVideo.style.left = 'auto';
-            loaderVideo.style.right = '60px';
-            loaderVideo.style.transform = 'translate(0%, -50%)';
-        } else if (window.matchMedia('(max-width: 1399px)').matches) {
-            loaderVideo.style.width = '450px';
-            loaderVideo.style.height = '450px';
-            loaderVideo.style.left = 'auto';
-            loaderVideo.style.right = '80px';
-            loaderVideo.style.transform = 'translate(0%, -50%)';
-        } else {
-            loaderVideo.style.width = '500px';
-            loaderVideo.style.height = '500px';
-            loaderVideo.style.top = '50%';
-            loaderVideo.style.left = 'auto';
-            loaderVideo.style.right = '100px';
-            loaderVideo.style.transform = 'translate(0%, -50%)';
-            loaderVideo.style.position = 'absolute';
-        }
-        document.body.classList.remove('overflow-hidden'); // body - overflow visible
-        document.documentElement.classList.remove('overflow-hidden'); // html - overflow visible
-    }, 3000); // Adjust the time as needed
+document.querySelector(".js-activate-global-blending").addEventListener("click", function() {
+  document.querySelector(".example-slider").classList.toggle("m--global-blending-active");
 });
-
-
-// ScrollMagic
-var controller = new ScrollMagic.Controller({ loglevel: 3 });
-new ScrollMagic.Scene({
-    triggerElement: "#section2",
-    triggerHook: "onEnter",
-    duration: "100%"
-}).setPin("#section1 .pinWrapper", {
-    pushFollowers: false
-}).addTo(controller);
-new ScrollMagic.Scene({
-    triggerElement: "#section2",
-    triggerHook: "onEnter",
-    duration: "200%"
-}).setPin("#section2 .pinWrapper", {
-    pushFollowers: false
-}).addTo(controller);
-new ScrollMagic.Scene({
-    triggerElement: "#section3",
-    triggerHook: "onEnter",
-    duration: "200%"
-}).setPin("#section3 .pinWrapper", {
-    pushFollowers: false
-}).addTo(controller);
-new ScrollMagic.Scene({
-    triggerElement: "#section4",
-    triggerHook: "onEnter",
-    duration: "100%"
-}).setPin("#section4 .pinWrapper", {
-    pushFollowers: false
-}).addTo(controller);
